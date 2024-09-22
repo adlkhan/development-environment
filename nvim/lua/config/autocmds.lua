9d9013674f11d28674f1e5ee373d5480aae484c7
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2024-09-21 23:42:23 -0500

    Create a group for the markdown autocommand

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,7 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
+vim.api.nvim_create_augroup("markdown_settings", { clear = true })
+
 vim.api.nvim_create_autocmd("FileType", {
+  group = "markdown_settings",
   pattern = { "markdown", "md" },
   callback = function()
     vim.b.minipairs_disable = true
