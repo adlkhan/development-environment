075eaaa4669674edb1eb43321beeb58c60bcecdb
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2024-10-16 19:03:02 -0500

    Add Svelte

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
     -- import/override with your plugins
     { import = "plugins" },
   },
