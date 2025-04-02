1c5a4c72b31b9042ccd24ec5d917aaf606fe0ab6
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2025-04-01 21:32:06 -0500

    Update asdfrc

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "ruby" },
+    ensure_installed = { "ruby", "html" },
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = { "ruby", "html" },
